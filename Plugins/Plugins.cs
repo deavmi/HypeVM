8f17154a89f8fa5f9669c9b6758d24875c49b741
Tristan B. Kildaire <deavmi@disroot.org>
2018-09-06 09:10:07 +0200

    Made more modular.

--- Plugins/Plugins.cs
+++ Plugins/Plugins.cs
@@ -8,16 +8,6 @@
  namespace HypeVM.Plugins
  {
 
-     using VM.Devices;
+     //TODO: Add more devices
 
-    //Re enable below for the error to revise
-    //  //A PC speaker device (TODO: kak statci DeviceInfo maybe?)
-    //  public sealed class PCSpeaker : Device
-    //  {
-    //      public override void poll() : base()
-    //      {
-    //          //TODO: Check whether or not we should beep the speaker
-    //          //and if so how many times
-    //      }
-    //  }
  }
