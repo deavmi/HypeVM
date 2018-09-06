6f675d8566bcd702a52e446d6151e714ec44745a
Tristan B. Kildaire <deavmi@disroot.org>
2018-09-06 22:16:05 +0200

    More todo for tomorrow.

--- Plugins/Devices/Filesystem device/FileSystem.cs
+++ Plugins/Devices/Filesystem device/FileSystem.cs
@@ -11,13 +11,13 @@ namespace HypeVM.Plugins.Devices.FileSystem
      * This device allows you to access the host's file system.
      *
      */
-     public sealed class NullDevice : Device
+     public sealed class FileSystemDevice : Device
      {
 
         //TODO: Try static kak maybe for DeviceInfo object we are passing? :) YES!
 
         //TODO: CHeck null plus string implemntation of operator and other kak (test3.txt)
-         public NullDevice() : base(new DeviceInfo("NullDevice " + null,"A device that does nothing"), null)
+         public FileSystemDevice() : base(new DeviceInfo("FileSystemDevice " + null,"A device that does nothing"), null)
          {
 
          }
@@ -26,8 +26,7 @@ namespace HypeVM.Plugins.Devices.FileSystem
          //Execute the null device (and do nothing)
          public override void poll()
          {
-            //Do nothing but say hey!
-            Console.Out.WriteLine("Hey I am a null device and I do fokol");
+            //TODO: Implement me
          }
      }
 
--- todo.md
+++ todo.md
@@ -4,3 +4,4 @@ Todo List
 * Make workspaces nice (starting with HypeVM)
 * Finsh machine internals
 * LocalMemory and various others
+* Add IDs to devices and a way for unified strutured information getting of them in memory
