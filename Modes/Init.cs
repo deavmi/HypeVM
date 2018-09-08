fcc1307cf407d057f69814c027bd0c9f61fbd1dd
Tristan B. Kildaire <deavmi@disroot.org>
2018-09-08 14:00:15 +0200

    Disabled some more old code.

--- Modes/Init.cs
+++ Modes/Init.cs
@@ -1,40 +1,40 @@
-namespace HypeVM.VM.Utils
-{
+// namespace HypeVM.VM.Utils
+// {
 
-    using System;
+//     using System;
 
-    using HypeVM.Slave.Servers.MemoryServer;
+//     using HypeVM.Slave.Servers.MemoryServer;
 
-    public sealed class Init
-    {
-        public static void bootMachine(VM vm, Bytecode.Bytecode bytecode)
-        {
-            RegisterState registers = vm.getRegisters();
+//     public sealed class Init
+//     {
+//         public static void bootMachine(VM vm, Bytecode.Bytecode bytecode)
+//         {
+//             RegisterState registers = vm.getRegisters();
 
-            //Set the codePointer register to 0 (FIXME: Remove)
-            registers.codePointer = 0;
+//             //Set the codePointer register to 0 (FIXME: Remove)
+//             registers.codePointer = 0;
 
-            //Set the codeBasePointer register to the end of the code
-            //TODO: Implement me
+//             //Set the codeBasePointer register to the end of the code
+//             //TODO: Implement me
 
-        }
-    }
+//         }
+//     }
 
-    public sealed class Utils
-    {
+//     public sealed class Utils
+//     {
 
-        public static void memTest(VM vm)
-        {
-            //Run through the memory of the VM printing it all out
-            Memory memory = vm.getMemory();
+//         public static void memTest(VM vm)
+//         {
+//             //Run through the memory of the VM printing it all out
+//             Memory memory = vm.getMemory();
 
-            for(short i = 0; i < memory.getSize(); i++)
-            {
-                Console.Out.WriteLine("MemTest[Address: "+i+"]: ByteVal<"+memory.getByte(i)+">");
-            }
+//             for(short i = 0; i < memory.getSize(); i++)
+//             {
+//                 Console.Out.WriteLine("MemTest[Address: "+i+"]: ByteVal<"+memory.getByte(i)+">");
+//             }
 
-            //Memory size
-            Console.Out.WriteLine("Memory size (bytes): " + memory.getSize());
-        }
-    }
-}
+//             //Memory size
+//             Console.Out.WriteLine("Memory size (bytes): " + memory.getSize());
+//         }
+//     }
+// }
