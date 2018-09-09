a3776a8a1dba4ccbb981605c11d8cff6caf4d0a1
Tristan B. Kildaire <deavmi@disroot.org>
2018-09-09 15:35:39 +0200

    Disabled devices for now. Want things to compile.

--- Plugins/Devices/Filesystem device/FileSystem.cs
+++ Plugins/Devices/Filesystem device/FileSystem.cs
@@ -1,36 +1,36 @@
-namespace HypeVM.Plugins.Devices.FileSystem
-{
+// namespace HypeVM.Plugins.Devices.FileSystem
+// {
 
-    using System;
+//     using System;
 
-    using VM.Devices;
+//     using VM.Devices;
 
-    /*
-     * FileSystem device
-     *
-     * This device allows you to access the host's file system.
-     *
-     */
-     public sealed class FileSystemDevice : Device
-     {
+//     /*
+//      * FileSystem device
+//      *
+//      * This device allows you to access the host's file system.
+//      *
+//      */
+//      public sealed class FileSystemDevice : Device
+//      {
 
-        //TODO: Try static kak maybe for DeviceInfo object we are passing? :) YES!
+//         //TODO: Try static kak maybe for DeviceInfo object we are passing? :) YES!
 
-        //TODO: CHeck null plus string implemntation of operator and other kak (test3.txt)
-         public FileSystemDevice() : base(new DeviceInfo("FileSystemDevice " + null,"A device that does nothing"), null)
-         {
+//         //TODO: CHeck null plus string implemntation of operator and other kak (test3.txt)
+//          public FileSystemDevice() : base(new DeviceInfo("FileSystemDevice " + null,"A device that does nothing"), null)
+//          {
 
-         }
+//          }
          
 
-         //Execute the null device (and do nothing)
-         public override void poll()
-         {
-            //TODO: Implement me
-         }
-     }
+//          //Execute the null device (and do nothing)
+//          public override void poll()
+//          {
+//             //TODO: Implement me
+//          }
+//      }
 
-     //Device information for the NullDevice
-     //TODO: Add this
+//      //Device information for the NullDevice
+//      //TODO: Add this
 
-}
+// }
