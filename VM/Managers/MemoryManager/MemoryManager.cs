296aa6ce00aa173c70c689d65cf1aff6d9822e8e
Tristan B. Kildaire <deavmi@disroot.org>
2018-09-09 15:42:03 +0200

    Alsmot compilable.

--- VM/Managers/MemoryManager/MemoryManager.cs
+++ VM/Managers/MemoryManager/MemoryManager.cs
@@ -5,19 +5,32 @@
  *
  */
 
- namespace HypeVM.Master.Memory
- {
+namespace HypeVM.Master.Memory
+{
 
-     using HypeVM.Master.Clients.Memory;
+    using HypeVM.Master.Clients.Memory; //TODO: Look at how this can be shortened and why
 
-     //Manager for the virtual machine's memory
-     public sealed class MemoryManager
-     {
+    //Manager for the virtual machine's memory
+    public sealed class MemoryManager
+    {
         //Construct a new MemoryManager with the given MemoryClient and memory size
         public MemoryManager(MemoryClient memoryClient, short memorySize)
         {
-            
+            //TODO: Implement me
         }
-     }
 
- }
+        //Set the value (byte) of at an address
+        public void setByte(short address, byte value)
+        {
+            //TODO: Implement me
+        }
+
+        //gets the value (byte) at the given address
+        public byte getByte(short address)
+        {
+            //TODO: Implement me
+            return 1;
+        }
+    }
+
+}
